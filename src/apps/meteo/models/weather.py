f820d113c750b7c1749f003aeb10eaa318670555
Ruslan <shaggybackend@gmail.com>
2023-05-10 15:57:00 +0200

    add api tests for get weather diff, add constraint for temp in weather data

--- src/apps/meteo/models/weather.py
+++ src/apps/meteo/models/weather.py
@@ -10,6 +10,52 @@ class WeatherData(models.Model):
     class Meta:
         verbose_name_plural = "Weather Data"
         db_table = "weather_data"
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(temperature_2m_max__gte=models.F("temperature_2m_min")),
+                name="temperature_2m_max_gte_temperature_2m_min",
+            ),
+            # TODO: fix this constraint's issue: refers to the nonexistent field
+            # models.CheckConstraint(
+            #     check=models.Q(
+            #         measured_weather__isnull=False, forecast_weather__isnull=False
+            #     ),
+            #     name="weather_data_should_be_forecast_or_measured",
+            # ),
+        ]
+
+    def clean(self) -> None:
+        if self.temperature_2m_max >= self.temperature_2m_min:
+            raise ValueError(
+                "temperature_2m_max must be greater than or equal to temperature_2m_min"
+            )
+        if not (self.is_measured or self.is_forecast):
+            raise ValueError("Weather data must be measured or forecast")
+
+    def __str__(self) -> str:
+        return (
+            f"{self.weather} |"
+            f"({self.temperature_2m_max}, {self.temperature_2m_min}, "
+            f"{self.precipitation_sum}, {self.windspeed_10m_max})"
+        )
+
+    @property
+    def is_measured(self) -> bool:
+        return hasattr(self, "measured_weather")
+
+    @property
+    def is_forecast(self) -> bool:
+        return hasattr(self, "forecast_weather")
+
+    @property
+    def weather(self) -> "Weather":
+        weather = getattr(self, "measured_weather", None) or getattr(
+            self, "forecast_weather", None
+        )
+        if not weather:
+            raise AttributeError("Weather data must be measured or forecast")
+
+        return weather
 
     @classmethod
     def fields(cls):
@@ -27,14 +73,14 @@ class Weather(models.Model):
     date = models.DateField()
     measured = models.OneToOneField(
         "meteo.WeatherData",
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
         related_name="measured_weather",
         null=True,
         blank=True,
     )
     forecast = models.OneToOneField(
         "meteo.WeatherData",
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
         related_name="forecast_weather",
         null=True,
         blank=True,
--- src/apps/meteo/tests/api/test_get_weather_difference.py
+++ src/apps/meteo/tests/api/test_get_weather_difference.py
@@ -3,15 +3,49 @@ from rest_framework import status
 from rest_framework.test import APIClient
 from rest_framework.reverse import reverse
 
+from src.apps.meteo.models.weather import Weather
+
 
 @pytest.mark.django_db
 def test_get_weather_difference_without_city(client: APIClient):
+    """
+    Test get weather difference without city, should return 400, city is required
+    """
     response = client.get(reverse("weather-difference"))
     assert response.status_code == status.HTTP_400_BAD_REQUEST, response.data
 
 
 @pytest.mark.django_db
-def test_get_weather_difference_with_city(client: APIClient, city, weather):
-    response = client.get(reverse("weather-difference"), {"city": city.name})
+def test_get_weather_difference_with_measured_weather_data_only(
+    client: APIClient, weather: Weather
+):
+    """Looking for weather with both measured and forecast data, so this should return empty list"""
+    weather.forecast = None
+    weather.save()
+    response = client.get(reverse("weather-difference"), {"city": weather.city.name})
+    assert response.status_code == status.HTTP_200_OK, response.data
+    assert len(response.data) == 0, response.data
+
+
+@pytest.mark.django_db
+def test_get_weather_difference_with_measured_and_forecast_weather_data(
+    client: APIClient, weather, weather_data_factory
+):
+    """
+    Success case for weather with both measured and forecast data
+    """
+    first_weather_data = weather_data_factory()
+    second_weather_data = weather_data_factory()
+    weather.measured = first_weather_data
+    weather.forecast = second_weather_data
+    weather.save()
+    response = client.get(reverse("weather-difference"), {"city": weather.city.name})
     assert response.status_code == status.HTTP_200_OK, response.data
     assert len(response.data) == 1, response.data
+    response_data = response.data[0]
+    assert response_data["city"] == weather.city.name, response_data
+    assert response_data["date"] == weather.date.strftime("%Y-%m-%d"), response_data
+    assert float(response_data["temperature_2m_max_diff"]) == round(
+        abs(weather.measured.temperature_2m_max - weather.forecast.temperature_2m_max),
+        2,
+    ), response_data
